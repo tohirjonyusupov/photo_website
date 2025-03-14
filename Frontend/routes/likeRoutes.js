1873defb8dd330dab1566484dbd9fd77bd898855
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2025-03-14 20:17:16 +0500

    initial commit

--- Frontend/routes/likeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const { like, getLikes } = require('../controllers/likeController')
-const likeRouter = express.Router()
-
-likeRouter.post('/', like)
-
-module.exports = likeRouter
